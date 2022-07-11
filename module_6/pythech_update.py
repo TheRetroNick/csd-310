bf2292404456f6824a047c35b3e75ac6e3c4103f
TheRetroNick <84204853+TheRetroNick@users.noreply.github.com>
2022-07-10 21:44:07 -0600

    Update pythech_update.py

--- module_6/pythech_update.py
+++ module_6/pythech_update.py
@@ -1,4 +1,4 @@
-# Assingment 6.2 - pytech_queries
+# Assingment 6.2 - pytech_update
 
 import pymongo
 
@@ -8,5 +8,5 @@ client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.cbez6.mongodb.n
 # select the database that you want to access
 db = client['pytech']
 
-# 
+# select the student ID info you want to update. $set will add the field and value if the field is not already present to be updated.
 db.students.update_one({'student_id': '1007'}, {'$set': {"lastName": "Smith"}})
