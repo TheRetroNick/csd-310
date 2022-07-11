f8d398f7ca6ab933867db80de2432b2a3aee03ff
TheRetroNick <84204853+TheRetroNick@users.noreply.github.com>
2022-07-10 21:22:10 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -10,6 +10,6 @@ db = client['pytech']
 # mycol = db['students'] If you want to put all 3 students in at once, but not using it here 
 
 # add each of the 3 new students to the database. Make sure you use 'students'
-db.students.insert_one({'student_id': '1007', 'firstName': 'fred'})
-db.students.insert_one({'student_id': '1008', 'firstName': 'barney'})
-db.students.insert_one({'student_id': '1009', 'firstName': 'dino'})
+fred_student_id = db.students.insert_one({'student_id': '1007', 'firstName': 'Fred', 'lastName': 'Flintstone'}).inserted_id
+barney_student_id = db.students.insert_one({'student_id': '1008', 'firstName': 'Farney', 'lastName': 'Rubble'}).inserted_id
+dino_student_id = db.students.insert_one({'student_id': '1009', 'firstName': 'Dino', 'lastName': 'theDinosaur'}).inserted_id
