ced6f47b2df3d8a49be74f20781ee27dac194de6
TheRetroNick <84204853+TheRetroNick@users.noreply.github.com>
2022-07-10 20:16:06 -0600

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,15 @@
+# Assingment 5.3 - pytech_queries
 
+import pymongo
+
+# call the server
+client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.cbez6.mongodb.net/?retryWrites=true&w=majority')
+
+# select the database that you want to access
+db = client['pytech']
+
+# query which student you want to find
+line = db.students.find_one({'student_id': '1008'})
+
+# output the query
+print(line)
