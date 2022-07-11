f57e097c5447beee3138fc25312ca2d84380107a
TheRetroNick <84204853+TheRetroNick@users.noreply.github.com>
2022-07-10 21:47:23 -0600

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -8,8 +8,12 @@ client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.cbez6.mongodb.n
 # select the database that you want to access
 db = client['pytech']
 
-# query which student you want to find
-doc = db.students.find_one({'student_id': '1008'})
 
-# output the query
+# query all 
+doc = db.students.find({})
+# query which student you want to find
+doc2 = db.students.find_one({'student_id': '1008'})
+
+# output both queries
 print(doc)
+print(doc2)
