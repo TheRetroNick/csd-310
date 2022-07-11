da8ff2daf5eb2bf7af6232ddd80effb253db17c0
TheRetroNick <84204853+TheRetroNick@users.noreply.github.com>
2022-07-10 21:39:59 -0600

    Update pythech_update.py

--- module_6/pythech_update.py
+++ module_6/pythech_update.py
@@ -1,1 +1,12 @@
+# Assingment 6.2 - pytech_queries
 
+import pymongo
+
+# call the server
+client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.cbez6.mongodb.net/?retryWrites=true&w=majority')
+
+# select the database that you want to access
+db = client['pytech']
+
+# 
+db.students.update_one({'student_id': '1007'}, {'$set': {"lastName": "Smith"}})
